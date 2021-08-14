52d353dbac3b9107d7faaa4ec9a7d1b151a1ae4f
Dev A <cag1982@protonmail.com>
2021-08-13 23:40:51 -0300

    fixes

--- src/widgets/Menu/PanelFooter.tsx
+++ src/widgets/Menu/PanelFooter.tsx
@@ -59,7 +59,7 @@ const SocialEntry = styled.div`
 `;
 
 const MusicIcon = styled.a`
-  color: #FFF;
+  color: rgba(255, 255, 255, .60);
 `
 
 const anim = keyframes`
@@ -122,13 +122,13 @@ const AudioPlayer = () => {
     <PriceBox>
       {isPlaying ? (
         <MusicIconPlaying key="music" href="#" role="button" onClick={(evt) => { evt.preventDefault(); setIsPlaying(!isPlaying); }}>
-          <svg width="32" height="32" aria-hidden="true" focusable="false" data-icon="music" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          <svg width="24" height="24" aria-hidden="true" focusable="false" data-icon="music" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
             <path fill="currentColor" d="M470.38 1.51L150.41 96A32 32 0 0 0 128 126.51v261.41A139 139 0 0 0 96 384c-53 0-96 28.66-96 64s43 64 96 64 96-28.66 96-64V214.32l256-75v184.61a138.4 138.4 0 0 0-32-3.93c-53 0-96 28.66-96 64s43 64 96 64 96-28.65 96-64V32a32 32 0 0 0-41.62-30.49z" />
           </svg>
         </MusicIconPlaying>
       ) : (
         <MusicIcon key="music" href="#" role="button" onClick={(evt) => { evt.preventDefault(); setIsPlaying(!isPlaying); }}>
-          <svg width="32" height="32" aria-hidden="true" focusable="false" data-icon="music" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          <svg width="24" height="24" aria-hidden="true" focusable="false" data-icon="music" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
             <path fill="currentColor" d="M470.38 1.51L150.41 96A32 32 0 0 0 128 126.51v261.41A139 139 0 0 0 96 384c-53 0-96 28.66-96 64s43 64 96 64 96-28.66 96-64V214.32l256-75v184.61a138.4 138.4 0 0 0-32-3.93c-53 0-96 28.66-96 64s43 64 96 64 96-28.65 96-64V32a32 32 0 0 0-41.62-30.49z" />
           </svg>
         </MusicIcon>
